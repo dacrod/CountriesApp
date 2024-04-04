d5cf28fd34c8f96ec37bcc73296438298fb6d56e
Daniel Cuarental Rodriguez <dcuarental@hiberus.com>
2024-04-04 11:31:28 +0200

    Nuevo componente searchbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesModule } from './countries/countries.module';
 
 const routes: Routes = [
-  {
+  /*{
     path: '',
     component: HomePageComponent
-  },
+  },*/
   {
     path: 'about',
     component: AboutPageComponent
@@ -19,11 +20,11 @@ const routes: Routes = [
   },
   {
     path: 'countries',
-    component: ContactPageComponent
+    loadChildren: () => import('./countries/countries.module').then( m=> m.CountriesModule )
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'countries'
   }
 ];
 
