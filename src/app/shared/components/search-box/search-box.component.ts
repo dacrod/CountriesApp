6b023b52a924a674e209a18b87c98ead0401fc35
Daniel Cuarental Rodriguez <dcuarental@hiberus.com>
2024-04-04 13:21:32 +0200

    Capital table

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'shared-search-box',
@@ -9,5 +10,11 @@ export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
 
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue( txtInput: string ): void {
+    this.onValue.emit(txtInput);
+  }
 
 }
